437d0f6d878cea2b104d327395d95c6821fba297
Oleg German <oleg.german@p2h.com>
2023-12-17 13:49:49 +0200

    init mvc

---
+++ Core/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core;
+
+class Router
+{
+    static protected array $routes = [], $params = [];
+
+    static public function add(string $route, array $params): void
+    {
+        static::$routes[$route] = $params;
+    }
+
+    static public function dispatch(string $uri): void
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+### DESCRIPTION
+
+Notes Application:
+
+- Users
+	- Login/Registration
+	- Authorization
+- Notes
+	- CRUD
+	- position(order)
+	- shared functionality (with another users)
+- Folders
+	- CRUD
+	- contains notes inside
+
+- API
+	- Notes (CRUD)
+	- Folders (CRUD)
+	- Auth - token
